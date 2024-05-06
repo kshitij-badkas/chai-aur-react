77040692042a508775633d7311dac19036f3354d
kshitij-badkas <kshitij_badkas@keemail.me>
2024-05-06 16:35:46 +0530

    Created own react library and JSX - 01vitereact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,14 @@
 import Chai from "./chai"
 function App() {
 
+  const username = "My Username";
+
   return (
-     <>
-     <h1>Chai aur React with Vite | KAB
-     
-    <Chai></Chai>
-    </h1>
+    <>
+      <h1>Chai aur React with Vite | KAB
+        <h2> {username} </h2>
+        <Chai></Chai>
+      </h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,11 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://duckduckgo.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit DuckDuckGo'
+// }
+
+const anotherElement = (
+  <a href="https://duckduckgo.com" target='_blank'>Visit DuckDuckGO!</a>
+)
+
+const anotherUser = "Chai Aur React";
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://duckduckgo.com', target: '_blank'},
+  'Click Me to Visit DuckDuckGo!',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  // <React.StrictMode>
-  <App />
-//</React.StrictMode>,
+  // <App />
+  // <MyApp/>
+  // MyApp()
+  // anotherElement
+  reactElement
+  
 )
