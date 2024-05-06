e1650d61fc75f3d696068a08be53139b2c844a7d
kshitij-badkas <kshitij_badkas@keemail.me>
2024-05-06 12:19:44 +0530

    Create project using create-react-app and create component in 01basicreact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Chai from './chai';
+
+function App() {
+  return (
+    <>
+      <Chai></Chai>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h2>Chai in create-react-app is ready!</h2>
+    )
+}
+
+export default Chai
